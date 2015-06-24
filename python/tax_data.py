438590e18e7316994529ff3c5431a60857f292cc
Stephen McMurtry <smcmurtry@gmail.com>
2015-06-24 18:58:28 -0400

    fixed up tax_data.py

--- python/tax_data.py
+++ python/tax_data.py
@@ -3,6 +3,8 @@
 
 #### Tax Data by Income
 
+# From Tim:
+# 
 # I just found a better link to the income data I was telling you about:
 # http://www.cra-arc.gc.ca/gncy/stts/t1fnl-eng.html
 #  
@@ -10,14 +12,12 @@
 #  
 # <p>there are a bunch of other tax filing related datasets available here: http://www.cra-arc.gc.ca/gncy/stts/menu-eng.html including 'T1 preliminary statistics' which are similar to the first link but run from 2006 to 2012.
 
-# In[3]:
+# In[1]:
 
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 get_ipython().magic(u'matplotlib inline')
-# import sys
-# sys.path.append('/Users/stephenmcmurtry/work/python_libraries')
 import useful as us
 from matplotlib.ticker import FuncFormatter
 
@@ -72,13 +72,17 @@ def bar_plot(values, labels, title, xformatter=None):
     return f, ax
 
 
-# In[11]:
+# In[3]:
 
 unit_figuring = {'$':{'$':'%', '-':'$'},
                  '-':{'$':'1/$', '-':'-'}}
 
 def by_income_bin(df, item_name, col_list): 
     return [int(df.query("Item == @item_name")[col]) for col in col_list]
+
+def vector_division(numerator, denomonator):
+    result = np.divide([float(i) for i in numerator], [float(i) for i in denomonator])
+    return result
 
 def plot_item(df, item_name, divisor_fn=None, col_type='dollar'):
     
@@ -93,14 +97,6 @@ def plot_item(df, item_name, divisor_fn=None, col_type='dollar'):
         col_names = number_cols
     else:
         print 'wtf!'
-        
-    def per_return(y):
-        y_per_return = np.divide([float(i) for i in y], [float(i) for i in number_of_returns_by_income])
-        return y_per_return
-
-    def per_dollar_income(y):
-        y_per_dollar_income = np.divide([float(i) for i in y], [float(i) for i in dollars_of_income_by_income])
-        return y_per_dollar_income
     
     if col_names == dollar_cols:
         numerator_unit = '$'
@@ -111,12 +107,13 @@ def plot_item(df, item_name, divisor_fn=None, col_type='dollar'):
         
     item_amount = by_income_bin(df, item_name, col_names)
     item_amount = np.multiply(1000., item_amount)
+    
     if divisor_fn == "per_return":
-        item_amount = per_return(item_amount)
+        item_amount = vector_division(item_amount, number_of_returns_by_income)
         denomonator_unit = '-'
         xlabel_text = 'per return '
     elif divisor_fn == "per_dollar_income":
-        item_amount = per_dollar_income(item_amount)
+        item_amount = vector_division(item_amount, dollars_of_income_by_income)
         denomonator_unit = '$'
         xlabel_text = 'per dollar of income '
     else:
@@ -139,90 +136,112 @@ def plot_item(df, item_name, divisor_fn=None, col_type='dollar'):
     return f, ax
 
 
+# In[9]:
+
+dollar_cols = [c for c in df.columns if c.find('$') != -1]
+number_cols = [c for c in df.columns if c.find('#') != -1]
+
+
+# In[10]:
+
+dollar_cols
+
+
+# In[11]:
+
+number_cols
+
+
 ##### Loading the data
 
-# In[12]:
+# In[13]:
 
 df = pd.DataFrame.from_csv('../data/cleaned_tax_data_2011.csv')
+del df["#"]
 
 
-# In[21]:
+# In[ ]:
+
+
+
+
+# In[6]:
 
 f, ax = plot_item(df, 'Total tax payable', divisor_fn="per_dollar_income")
 
 
 ##### Task: Make a bar graph that shows the number of tax returns in each income bin
 
-# In[15]:
+# In[25]:
 
 f, ax = plot_item(df, 'Total income assessed')
 
 
-# In[16]:
+# In[26]:
 
 f, ax = plot_item(df, 'Total number of returns', col_type='number')
 
 
 ##### Task: Plot the tax income for each income bin
 
-# In[18]:
+# In[27]:
 
 f, ax = plot_item(df, 'Total tax payable', divisor_fn="per_return")
 
 
 ##### More exploration
 
-# In[190]:
+# In[28]:
 
-f, ax = plot_item(df, "Children's fitness amount", dollar_cols, divisor_fn=per_return)
-f.savefig('childrens_fitness_by_income.png', bbox_inches='tight')
-
-
-# In[192]:
-
-f, ax = plot_item(df, "Children's arts amount", dollar_cols, divisor_fn=per_return)
+f, ax = plot_item(df, "Children's fitness amount", divisor_fn="per_return")
+#f.savefig('childrens_fitness_by_income.png', bbox_inches='tight')
 
 
-# In[193]:
+# In[29]:
 
-f, ax = plot_item(df, "Net federal tax", dollar_cols, divisor_fn=per_return)
-
-
-# In[194]:
-
-f, ax = plot_item(df, "Net Provincial Tax", dollar_cols, divisor_fn=per_return)
+f, ax = plot_item(df, "Children's arts amount", divisor_fn="per_return")
 
 
-# In[202]:
+# In[30]:
 
-f, ax = plot_item(df, 'RRSP deduction', dollar_cols, divisor_fn=per_return)
+f, ax = plot_item(df, "Net federal tax", divisor_fn="per_return")
 
 
-# In[19]:
+# In[31]:
+
+f, ax = plot_item(df, "Net Provincial Tax", divisor_fn="per_return")
+
+
+# In[32]:
+
+f, ax = plot_item(df, 'RRSP deduction', divisor_fn="per_return")
+
+
+# In[33]:
 
 f, ax = plot_item(df, 'Interest and other investment income', divisor_fn="per_return")
 #f.savefig('investment_income_by_income.png', bbox_inches='tight')
 
 
-# In[196]:
+# In[34]:
 
-f, ax = plot_item(df, 'Employment income', dollar_cols, divisor_fn=per_return)
-
-
-# In[197]:
-
-f, ax = plot_item(df, 'Net income after adjustments', dollar_cols, divisor_fn=per_return)
+f, ax = plot_item(df, 'Employment income', divisor_fn="per_return")
 
 
-# In[199]:
+# In[35]:
 
-f, ax = plot_item(df, 'Carrying charges and interest expenses', dollar_cols, divisor_fn=per_return)
-
-
-# In[201]:
-
-f, ax = plot_item(df, 'Interest paid on student loans', dollar_cols, divisor_fn=per_return)
+f, ax = plot_item(df, 'Net income after adjustments', divisor_fn="per_return")
 
 
-# In[4]:
+# In[36]:
+
+f, ax = plot_item(df, 'Carrying charges and interest expenses', divisor_fn="per_return")
+
+
+# In[37]:
+
+f, ax = plot_item(df, 'Interest paid on student loans', divisor_fn="per_return")
+
+
+# In[5]:
 
