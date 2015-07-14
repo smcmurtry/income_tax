f9c5cea23cf0d1c4ea8602a462f12ec46a21f6d9
Stephen McMurtry <smcmurtry@gmail.com>
2015-07-14 15:06:01 -0400

    added item categories

---
+++ python/item_categories.py
@@ -0,0 +1,245 @@
+item_categories = [
+['CPP contributions on self-employment', 'non-refundable tax credits']
+
+ ,['CPP or QPP benefits', 'income items']
+
+ ,['CPP or QPP contributions', 'non-refundable tax credits']
+
+ ,['CPP or QPP contributions on employment', 'non-refundable tax credits']
+
+ ,['CPP or QPP contributions on self-employment', 'non-refundable tax credits']
+
+ ,['Canada employment amount', 'non-refundable tax credits']
+
+ ,['Canadian Forces personnel and police deduction', 'deduction items']
+
+ ,['EI benefits', 'income items']
+
+ ,['EI premiums', 'non-refundable tax credits']
+
+ ,['EI premiums on self-employment and other eligible earnings', 'non-refundable tax credits']
+
+ ,['EI premiums payable on self-employment', 'non-refundable tax credits']
+
+ ,['Net income before adjustments', 'income items']
+
+ ,['Old Age Security Pension', 'income items']
+
+ ,['PPIP premiums paid', 'non-refundable tax credits']
+
+ ,['PPIP premiums payable on employment income', 'non-refundable tax credits']
+
+ ,['PPIP premiums payable on self-employment income', 'non-refundable tax credits']
+
+ ,['RRSP deduction', 'deduction items']
+
+ ,['RRSP income', 'income items']
+
+ ,['Registered Disability Savings Plan income', 'income items']
+
+ ,['Registered Pension Plan contributions', 'deduction items']
+
+ ,['Saskatchewan Pension Plan deduction', 'deduction items']
+
+ ,['Universal Child Care Benefit', 'income items']
+
+ ,['Universal Child Care Benefit repayment', 'deduction items']
+
+ ,['Yukon first nations tax', 'tax payable']
+
+ ,['additional deductions', 'deduction items']
+
+ ,['adoption expenses', 'non-refundable tax credits']
+
+ ,['age amount', 'non-refundable tax credits']
+
+ ,['alternative minimum tax payable', 'tax payable']
+
+ ,['amount for children 17 and under', 'non-refundable tax credits']
+
+ ,['amount for eligible dependant','non-refundable tax credits']
+
+ ,['amount for infirm dependants age 18 or older', 'non-refundable tax credits']
+
+ ,['amounts transferred from spouse or common-law partner', 'non-refundable tax credits']
+
+ ,['annual union, professional, or like dues', 'deduction items']
+
+ ,['annuity income', 'income items']
+
+ ,['basic federal tax', 'tax payable']
+
+ ,['basic personal amount', 'non-refundable tax credits']
+
+ ,['business investment loss', 'deduction items']
+
+ ,['capital gains deduction', 'deduction items']
+
+ ,['caregiver amount', 'non-refundable tax credits']
+
+ ,['carrying charges and interest expenses', 'deduction items']
+
+ ,['child care expenses', 'deduction items']
+
+ ,["children's arts amount", 'non-refundable tax credits']
+
+ ,["children's fitness amount", 'non-refundable tax credits']
+
+ ,['clergy residence deduction', 'deduction items']
+
+ ,['commissions (from employment)', 'income items']
+
+ ,['deduction for PPIP premiums on self-employment income', 'deduction items']
+
+ ,['deduction for elected split-pension amount', 'deduction items']
+
+ ,['deductions for CPP/QPP contributions on self-employment and other earnings', 'deduction items']
+
+ ,['disability amount', 'non-refundable tax credits']
+
+ ,['disability amount transferred from a dependant', 'non-refundable tax credits']
+
+ ,['disability supports deductions', 'deduction items']
+
+ ,['donations and gifts', 'non-refundable tax credits']
+
+ ,['elected split-pension amount', 'income items']
+
+ ,['eligible cultural and ecological gifts', 'non-refundable tax credits']
+
+ ,['employee home relocation loan deduction', 'deduction items']
+
+ ,['employment income', 'income items']
+
+ ,['exploration and development expenses', 'deduction items']
+
+ ,['farming/fishing losses of prior years', 'deduction items']
+
+ ,['federal dividend tax credit', 'non-refundable tax credits']
+
+ ,['federal foreign tax credit', 'non-refundable tax credits']
+
+ ,['federal political contribution tax credit', 'non-refundable tax credits']
+
+ ,["home buyers' amount", 'non-refundable tax credits']
+
+ ,['home renovation expenses', 'non-refundable tax credits']
+
+ ,['interest and other investment income', 'income items']
+
+ ,['interest paid on student loans', 'non-refundable tax credits']
+
+ ,['investment tax credit', 'non-refundable tax credits']
+
+ ,['labour-sponsored funds tax credit', 'non-refundable tax credits']
+
+ ,['limited partnership losses of other years', 'deduction items']
+
+ ,['losses from other years', 'deduction items']
+
+ ,['medical expenses', 'non-refundable tax credits']
+
+ ,['minimum tax carryover', 'tax payable']
+
+ ,['moving expenses', 'deduction items']
+
+ ,['net business income', 'income items']
+
+ ,['net capital losses of other years', 'deduction items']
+
+ ,['net commission income', 'income items']
+
+ ,['net farming income', 'income items']
+
+ ,['net federal supplements', 'income items']
+
+ ,['net federal tax', 'tax payable']
+
+ ,['net fishing income', 'income items']
+
+ ,['net income after adjustments', 'income items']
+
+ ,['net partnership income', 'income items']
+
+ ,['net professional income', 'income items']
+
+ ,['net provincial or territorial tax', 'tax payable']
+
+ ,['net rental income', 'income items']
+
+ ,['non-capital losses of other years', 'deduction items']
+
+ ,['northern residents deductions', 'deduction items']
+
+ ,['number of non-taxable returns', 'number items']
+
+ ,['number of taxable returns', 'number items']
+ 
+ ,['other deductions', 'deduction items']
+
+ ,['other employment expenses', 'deduction items']
+
+ ,['other employment income', 'income items']
+
+ ,['other federal non-refundable tax credits', 'non-refundable tax credits']
+
+ ,['other income', 'income items']
+
+ ,['other payments deductions', 'deduction items']
+
+ ,['other pensions or superannuation', 'income items']
+
+ ,['overseas employment tax credit', 'non-refundable tax credits']
+
+ ,['pension income amount', 'non-refundable tax credits']
+
+ ,['public transit amount', 'non-refundable tax credits']
+
+ ,['security options deductions', 'deduction items']
+
+ ,['social assistance payments', 'income items']
+
+ ,['social benefits repayment', 'tax payable']
+
+ ,['spouse or common-law partner amount', 'non-refundable tax credits']
+
+ ,['spouse, eligible dependant and adoption amount', 'non-refundable tax credits']
+
+ ,['support payments made', 'deduction items']
+
+ ,['support payments received', 'income items']
+
+ ,['tax-exempt income', 'income items']
+
+ ,['taxable amount of dividends', 'income items']
+
+ ,['taxable capital gains', 'income items']
+
+ ,['taxable income assessed', 'income items']
+
+ ,['total deductions before adjustments', 'deduction items']
+
+ ,['total deductions from net income', 'deduction items']
+
+ ,['total federal non-refundable tax credits', 'non-refundable tax credits']
+
+ ,['total income assessed', 'income items']
+
+ ,['total number of returns', 'number items']
+ 
+ ,['total tax credit on donations and gifts', 'non-refundable tax credits']
+
+ ,['total tax credits on personal amounts', 'non-refundable tax credits']
+
+ ,['total tax payable', 'tax payable']
+
+ ,['tuition, education, and textbook amounts', 'non-refundable tax credits']
+
+ ,['tuition, education, and textbook amounts transferred from a child', 'non-refundable tax credits']
+
+ ,['tuition, education, textbook amounts and interest on student loans', 'non-refundable tax credits']
+
+ ,["volunteer firefighters' amount", 'non-refundable tax credits']
+
+ ,["workers' compensation benefits", 'income items']
+]
